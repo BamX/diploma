08ee9d115d95d11dee630b84d6c1b47edc5136e8
Nikolay Volosatov <bamx23@yandex-team.ru>
2015-05-22 23:31:04 +0300

    Fix plot creation

--- output/createplot.sh
+++ output/createplot.sh
@@ -1,16 +1,16 @@
 #!/usr/local/bin/gnuplot -persist
 set encoding utf8
-set term png size 800,400
+set term png size 1200,700
 set output 'plot.png'
 
 set grid
-set title 'График охлаждения слитка'
+set title 'График температуры слитка'
 set xlabel 'Время, с'
-set ylabel 'Температура, с'
+set ylabel 'Температура, K'
 
 set datafile separator ','
-plot 'view.csv' using 1:2 title 'центр, низ' with lines, \
-     'view.csv' using 1:3 title 'центр, центр' with lines, \
-     'view.csv' using 1:4 title 'лево, центр' with lines, \
-     'view.csv' using 1:5 title 'ноль' with lines, \
-     'view.csv' using 1:6 title 'граница' with lines
+plot 'view.csv' using 1:2 title '(0.0, 0.115)' with lines, \
+     'view.csv' using 1:3 title '(0.14, 0.115)' with lines, \
+     'view.csv' using 1:4 title '(0.14, 0.0)' with lines, \
+     'view.csv' using 1:5 title '(0.0, 0.0)' with lines, \
+     'view.csv' using 1:6 title '(0.15, 0.125)' with lines
